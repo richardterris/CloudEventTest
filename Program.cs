e4e250cdab0aa4dffb1aac498ff57969253e1b17
Richard Terris <rterris@it-61816.nba-hq.com>
2024-12-17 14:25:39 -0500

    json formatter

--- Program.cs
+++ Program.cs
@@ -2,25 +2,31 @@ using System.Text.Json;
 using CloudNative.CloudEvents;
 using Newtonsoft.Json.Linq;
 using CloudTest;
+using CloudNative.CloudEvents.NewtonsoftJson;
+using CloudNative.CloudEvents.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/game", async(HttpContext context, CloudEvent cloudEvent) => {
-     if (cloudEvent == null)
+app.MapPost("/game", async(HttpRequest request, CloudEvent cloudEvent) => {
+
+    CloudEventFormatter formatter = new JsonEventFormatter();
+    CloudEvent ev = await request.ToCloudEventAsync(formatter);
+
+     if (ev == null)
         {
             return Results.BadRequest("CloudEvent was not provided or could not be deserialized.");
         }
             
-        if (cloudEvent.Data is JsonElement dataElement)
-        {
-            string jsonString = dataElement.GetRawText();
-            JObject dataAsJObject = JObject.Parse(jsonString);
-            var result = dataAsJObject.ToObject<GameUpdateEvent>();
-            Console.WriteLine($"Game Id: {result?.GameId}");
-        }
+        // if (cloudEvent.Data is JsonElement dataElement)
+        // {
+        //     string jsonString = dataElement.GetRawText();
+        //     JObject dataAsJObject = JObject.Parse(jsonString);
+        //     var result = dataAsJObject.ToObject<GameUpdateEvent>();
+        //     Console.WriteLine($"Game Id: {result?.GameId}");
+        // }
 
         return Results.Ok(); 
 });
